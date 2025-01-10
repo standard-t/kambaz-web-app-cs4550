9fd03c0e34ce38e68c35d56b80c3709f31003370
standard-t <147738136+standard-t@users.noreply.github.com>
2025-01-10 11:25:28 -0500

    introducing headings

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,3 +1,9 @@
 export default function Lab1() {
-    return <h2>Lab1</h2>;
+    return (
+        <div>
+            <h2> Lab 1</h2>
+            <h3> Headings</h3>
+            <p> Headings allow you to break up the content into multiple sections</p>
+        </div>
+    );
 }
