4ac928c76c2863c703c4f8abcab8235172f7bdc5
standard-t <147738136+standard-t@users.noreply.github.com>
2025-02-16 20:05:19 -0700

    a3 finished

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -4,16 +4,17 @@ import { Link } from "react-router-dom";
 export default function TOC() {
     const { pathname } = useLocation();
     return (
-        <Nav variant="pills" id="wd-toc">
-            <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab1" id="wd-a1"
-                active={pathname.includes("Lab1")}> Lab 1 </Nav.Link> </Nav.Item>
-            <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab2" id="wd-a2"
-                active={pathname.includes("Lab2")}> Lab 2 </Nav.Link> </Nav.Item>
-            <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab3" id="wd-a3"
-                active={pathname.includes("Lab3")}> Lab 3 </Nav.Link> </Nav.Item>
-            <Nav.Item> <Nav.Link as={Link} to="/Kambaz" id="wd-a3"> Kambaz </Nav.Link> </Nav.Item>
-            <Nav.Item> <Nav.Link href="https://github.com/standard-t/kambaz-web-app-cs4550/tree/a3" target="_blank">GitHub Repo</Nav.Link> </Nav.Item>
-        </Nav>
+        <><h3>Tara Standard <br /> CS4550 Sec 02</h3><br />
+            <Nav variant="pills" id="wd-toc">
+                <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab1" id="wd-a1"
+                    active={pathname.includes("Lab1")}> Lab 1 </Nav.Link> </Nav.Item>
+                <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab2" id="wd-a2"
+                    active={pathname.includes("Lab2")}> Lab 2 </Nav.Link> </Nav.Item>
+                <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab3" id="wd-a3"
+                    active={pathname.includes("Lab3")}> Lab 3 </Nav.Link> </Nav.Item>
+                <Nav.Item> <Nav.Link as={Link} to="/Kambaz" id="wd-a3"> Kambaz </Nav.Link> </Nav.Item>
+                <Nav.Item> <Nav.Link id="wd-github" href=" https://github.com/standard-t/kambaz-web-app-cs4550/tree/a3" target="_blank">GitHub Repo</Nav.Link> </Nav.Item>
+            </Nav ></>
     );
 }
 
