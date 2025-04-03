5f8d45d0d87e4c29e26e4191862c30c1d69591b9
standard-t <147738136+standard-t@users.noreply.github.com>
2025-04-03 18:33:52 -0400

    unused import

--- src/Kambaz/Account/Users.tsx
+++ src/Kambaz/Account/Users.tsx
@@ -58,6 +58,7 @@ export default function Users() {
                 <FaPlus className="me-2" />
                 Users
             </button>
+            <h3>Search:{name}</h3>
             <FormControl onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people"
                 className="float-start w-25 me-2 wd-filter-by-name" />
             <select value={role} onChange={(e) => filterUsersByRole(e.target.value)}
