11b79a4c71e85bb06385c35d8dd96dbc8aff2341
standard-t <147738136+standard-t@users.noreply.github.com>
2026-02-10 14:51:27 -0500

    spacing adjusted

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -39,8 +39,7 @@ export default function Signin() {
                 variant="primary" className="w-100" > Sign in </Button>
             <Link id="wd-signup-link" to="/Kambaz/Account/Signup">
                 Sign up </Link>
-            <body><strong>Admin Username: </strong>Ada <strong>Admin Password: </strong>123</body>
-            <br/> <body>Or Sign Up for Student Account!</body>
+            <body><strong>Admin Username: </strong>Ada <strong>Admin Password: </strong>123 <br/> Or Sign Up for Student Account!</body>
         </div>
     );
 }
